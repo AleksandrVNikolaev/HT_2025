d35b86c61c65d7e383fa4aa73928e05fb3b1f4db
Aleksandr Nikolaev <aleksandr.v.nikolaev@gmail.com>
2025-06-12 19:45:01 +0300

    wip

--- lesson_2.py
+++ lesson_2.py
@@ -1,9 +1,45 @@
 import time
-from lesson_2_dzata import courts
+from traceback import format_exception
+from lesson_2_dzdata import respondents, courts
 
-a = 0
+a = 10
 
-while True:
-    a+=1
+def gen_court_header(court: dict) -> str:
+        header = f'В: {court["court_name"]}\n' \
+             f'Адрес: {court["court_address"]}\n'
+        return header
+
+def gen_respondent_header(respondent: dict) -> str:
+    a = 15
     print(a)
-    time.sleep(1)
+    header = f'Ответчик: {respondent["short_name"]}\n' \
+             f'ИНН: {respondent["inn"]} ОГРН {respondent["ogrn"]}\n' \
+             f'Адрес: {respondent["address"]}\n' \
+            f"------------"
+    return header
+
+a = 'sdfdsfsvdsdvd'
+print(a.lower())
+print('stop')
+
+
+def main():
+    print('start')
+
+    print(a)
+    court_mapping = {i['court_code']: i for i in courts}
+    for i in respondents:
+        try:
+            code = i['case_number'][:3]
+            court =  court_mapping[code]
+            court_header = gen_court_header(court)
+            print(court_header)
+            respondent_header = gen_respondent_header(i)
+            print(respondent_header)
+        except Exception as exc:
+            format_exception(exc)
+            continue
+
+if __name__ == "__main__":
+    main()
+    print('stop')
