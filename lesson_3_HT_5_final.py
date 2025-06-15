3487c9e7273932d72a91b58b170ee6c3c205e385
Aleksandr Nikolaev <aleksandr.v.nikolaev@gmail.com>
2025-06-15 21:16:42 +0300

    wip

--- lesson_3_HT_5_final.py
+++ lesson_3_HT_5_final.py
@@ -3,7 +3,7 @@
 import json
 import csv
 
-# Задание 1. ИНН из файла traders.txt
+# Задание 5.1.1. ИНН из файла traders.txt
 with open('traders.txt', 'r') as file:
     inn_list = [line.strip() for line in file if line.strip()]
 
@@ -11,11 +11,11 @@ print('Список ИНН из файла traders.txt:')
 for inn in inn_list:
     print(inn)
 
-# Задание 2. Организации из traders.json
+# Задание 5.1.2. Организации из traders.json
 with open('traders.json', 'r') as file:
     orgs = json.load(file)
 
-# Задание 3. Фильтрация организаций по ИНН
+# Задание 5.1.3. Фильтрация организаций по ИНН
 filtered_orgs = []
 
 print('\nНайденные по ИНН организации:')
@@ -30,10 +30,13 @@ for org in orgs:
         }
         filtered_orgs.append(filtered)
 
-# Задание 4. CSV-файл с отфильтрованными организациями
+# Задание 5.1.4. CSV-файл с отфильтрованными организациями
 with open('traders.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.DictWriter(file, fieldnames=['short_name', 'inn', 'ogrn', 'address'], delimiter='|')
     writer.writeheader()
     writer.writerows(filtered_orgs)
 
 print('\nСохранено в файл traders.csv')
+
+
+# Домашнее задание № 5. Часть 2
