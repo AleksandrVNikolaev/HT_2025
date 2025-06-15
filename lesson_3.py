6f9a0aaa35308ab9a98fe3977e7f86471c236e56
Aleksandr Nikolaev <aleksandr.v.nikolaev@gmail.com>
2025-06-15 16:03:01 +0300

    wip

--- lesson_3.py
+++ lesson_3.py
@@ -1,14 +1,15 @@
-
+import json
 
 abs_path = r'C:\Users\Lenovo\PycharmProjects\PythonProject\traders.txt'
-rel_path = r'traders.txt'
+rel_path = r'C:\Users\Lenovo\PycharmProjects\PythonProject\1000_efrsb_messages.json'
 
-
-file = open(rel_path, 'r')
-data_1 = file.readlines()
-file.close()
 
 with open(r'C:\Users\Lenovo\PycharmProjects\PythonProject\traders.txt', 'r') as file:
     data_2 = file.readlines()
+    pass
+
+with open(rel_path, 'r') as file:
+    data_2 = json.load(file)
+    pass
 
 print('stop')
