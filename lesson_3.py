0e43d5e8fe2282b66ce8904bd7f83451a4ff6564
Aleksandr Nikolaev <aleksandr.v.nikolaev@gmail.com>
2025-06-15 13:22:23 +0300

    wip

--- lesson_3.py
+++ lesson_3.py
@@ -1,7 +1,14 @@
 
 
-file = open('C:\Users\Lenovo\PycharmProjects\PythonProject\traders.txt', 'r')
-data = file
+abs_path = r'C:\Users\Lenovo\PycharmProjects\PythonProject\traders.txt'
+rel_path = r'traders.txt'
 
 
-pass
+file = open(rel_path, 'r')
+data_1 = file.readlines()
+file.close()
+
+with open(r'C:\Users\Lenovo\PycharmProjects\PythonProject\traders.txt', 'r') as file:
+    data_2 = file.readlines()
+
+print('stop')
